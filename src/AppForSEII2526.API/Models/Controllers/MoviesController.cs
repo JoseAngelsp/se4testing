4d2f0ea8093381817e1b24d2a1ae54b8c6a56f98
JoseAngelsp <JoseAngel.Sotoca@alu.uclm.es>
2025-10-02 09:05:27 +0200

    Developing moviescontroller (#7)

---
+++ src/AppForSEII2526.API/Models/Controllers/MoviesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Models.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+    }
+}
